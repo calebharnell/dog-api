4df18b3110fcb97e6922f314d0d76106ac394ff7
calebharnell <calebharnell@hotmail.com>
2017-11-30 11:00:09 +1000

    Install express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Server!!!');
+const express = require('express');
+const app = express();
+
+const dogs = ['Doggo', 'Pupper', 'Woofer']
+
+app.get('/api/dogs', (req, res) => {
+  res.send(dogs)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
